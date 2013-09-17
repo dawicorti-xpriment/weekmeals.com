072db6c73ddd07ac139cd130b904dd8b4b7f8852
David Corticchiato <dawi@drone>
2013-09-17 14:29:45 +0200

    full grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,43 @@ module.exports = function(grunt) {
 
 
     grunt.initConfig({
+        clean: ['build', 'public/weekmeals.js'],
+        bower: {install: {}},
         amdwrap: {
             usingDynamicExpansion: {
                 expand: true,
-                cwd: "lib/client",
-                src: ["*.js", "**/*.js"],
-                dest: "build/amd"
+                cwd: 'lib/client',
+                src: ['*.js', '**/*.js'],
+                dest: 'build/amd'
+            }
+        },
+        requirejs: {
+            compile: {
+                options: {
+                    baseUrl: './',
+                    paths: {
+                        jquery: 'bower_components/jquery/jquery',
+                        underscore: 'bower_components/underscore/underscore',
+                        backbone: 'bower_components/backbone/backbone',
+                    },
+                    shim: {
+                        jquery: {exports: '$'},
+                        underscore: {exports: '_'},
+                        backbone: {exports: 'Backbone', deps: ['jquery', 'underscore']}
+                    },
+                    optimize: 'none',
+                    name: './build/amd/index.js',
+                    out: 'public/weekmeals.js',
+                    deps: ['bower_components/requirejs/require']
+                }
             }
         }
     });
+    
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-amd-wrap');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
 
-    grunt.loadNpmTasks("grunt-amd-wrap");
-
-    grunt.registerTask('default', ['amdwrap']);
+    grunt.registerTask('default', ['bower', 'amdwrap', 'requirejs']);
 };
